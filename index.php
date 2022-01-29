423eaca1aa97f79caf759ab25aa590c4c330941c
atsushi-okamoto <a.okamoto20@gmail.com>
2022-01-29 23:49:51 +0900

    Edit: index文字サイズ

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   <body>
     <?php include('navbar.php'); ?>
-    <main role="main" class="container" style="padding:60px 15px 0">
+    <main role="main" class="container" style="padding:50px 10px 0">
       <div>
         <h1>ARCH_BLOG</h1>
         <p><?php echo $info; ?></p>
