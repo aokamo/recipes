1bc547b3871018ecf8b018924197f665ecf935bd
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-02 19:05:59 +0900

    Edit: dbc-接続確認メッセージ

--- dbc.php
+++ dbc.php
@@ -1,11 +1,18 @@
 <?php
 
-$dsn = 'mysql:host=localhost;dbname=recipes;charset=utf8'
+$dsn = 'mysql:host=localhost;dbname=recipes;charset=utf8';
 $user = 'recipes_user';
 $pass = 'Recipes1234';
 
-$dbh = new PDO($dsn,$user,$pass);
-
-var_dump($dbh);
+try{
+    $dbh = new PDO($dsn,$user,$pass[
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+    echo '接続成功';
+    $dbh = null;
+} catch(PDOException $e) {
+    echo '接続失敗'. $e->getMessage();
+    exit();
+};
 
 ?>
