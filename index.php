983df866643501e173438e0951cecdb42cbc1245
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-02 15:03:16 +0900

    Fix: index-head

--- index.php
+++ index.php
@@ -3,7 +3,9 @@
 <html lang="ja">
   <head>
     <title>arch_blog</title>
+    <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  </head>
   <body>
     <?php include('navbar.php'); ?>
     <main role="main" class="container" style="padding:50px 10px 0">
