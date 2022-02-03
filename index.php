ddd2195feee526ce3b72c7eee82ea7ede0bef7a5
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-03 21:52:43 +0900

    Edit: header追加

--- index.php
+++ index.php
@@ -7,14 +7,16 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   </head>
   <body>
-    <?php include('navbar.php'); ?>
+    <header>
+      　<?php include('navbar.php'); ?>
+    </header>
     <main role="main" class="container" style="padding:60px 15px 0">
       <div class=info>
         <h1>ARCH_BLOG</h1>
         <p><?php echo $info; ?></p>
       </div>
       <div class=index>
-        
+
       </div>
     </main>
   </body>
