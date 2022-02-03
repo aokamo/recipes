b83fcaf0ab2e00e8653ca4ffee3e7938736081d8
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-03 22:15:42 +0900

    Edit: index-head

--- index.php
+++ index.php
@@ -2,7 +2,10 @@
 <!doctype html>
 <html lang="ja">
   <head>
-    <title>arch_blog</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>recipes</title>
     <!-- bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
   </head>
