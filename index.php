4670aabd873a3ede3fd23999e3dc6de127213152
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-04 21:25:03 +0900

    Edit: index-関数化（postsデータ取得）

--- index.php
+++ index.php
@@ -1,46 +1,54 @@
-<!-- データベース接続 -->
 <?php
+
 // 関数
-// 1関数1機能←可読性・テストのしやすさ
+// 1関数1機能にする　←　可読性・テストのしやすさ
 
 // 関数（データベース接続）
 // 引数：なし
 // 返り値：接続結果を返す
 function dbConnect(){
-// 接続に成功した場合
-try{
-  $dbh = new PDO($dsn,$user,$pass,[
-    // 例外を出力
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-  ]);
-// 接続に失敗した場合
-} catch(PDOException $e) {
-  echo '接続失敗'. $e->getMessage();
-  exit();
-};
-return $dbh;
+  // db接続にDSN（data source name）ユーザー、パスワードを定義
+  $dsn = 'mysql:host=localhost;dbname=recipes;charset=utf8';
+  $user = 'recipes_user';
+  $pass = 'Recipes1234';
+
+  // 接続に成功した場合
+  try{
+    $dbh = new PDO($dsn,$user,$pass,[
+      // 例外を出力
+      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    ]);
+  // 接続に失敗した場合
+  } catch(PDOException $e) {
+    echo '接続失敗'. $e->getMessage();
+    exit();
+  };
+  // 関数実行
+  return $dbh;
 }
 
+// 関数（データ取得）
+// 引数：なし
+// 返り値：取得したデータ
+function getAllPosts(){
+  // db接続
+  $dbh = dbConnect();
+  // ①SQLの準備
+  $sql = 'SELECT * FROM posts';
+  // ②SQL実行
+  $stmt = $dbh->query($sql);
+  // ③SQLの結果を受け取る
+  $result = $stmt->fetchall(pdo::FETCH_ASSOC);
+  // var_dump($result);←取得データ確認コード
+  // 関数実行
+  return $result;
+  // db接続終了
+  $dbh = null;
+}
 
-// ①SQLの準備
-$sql = 'SELECT * FROM posts';
-// ②SQL実行
-$stmt = $dbh->query($sql);
-// ③SQLの結果を受け取る
-$result = $stmt->fetchall(pdo::FETCH_ASSOC);
-// var_dump($result);←取得データ確認コード
-
-// db接続終了
-$dbh = null;
-
-
-
-// db接続にDSN（data source name）ユーザー、パスワードを定義
-$dsn = 'mysql:host=localhost;dbname=recipes;charset=utf8';
-$user = 'recipes_user';
-$pass = 'Recipes1234';
-
-
+// function一覧
+// 取得したデータを表示
+$postsData = getAllPosts();
 
 ?>
 
@@ -73,7 +81,7 @@ $pass = 'Recipes1234';
             <th>body</th>
             <th>created_at</th>
           </tr>
-          <?php foreach($result as $column): ?>
+          <?php foreach($postsData as $column): ?>
             <!-- ＊foreachにはコロン -->
           <tr>
             <td><?php echo $column['id']; ?></td>
