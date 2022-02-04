2b4984e739ab62168e6dc0742fdf2f6fbbc2a6d6
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-04 21:02:54 +0900

    Edit: index-関数化（データベース接続）

--- index.php
+++ index.php
@@ -1,33 +1,46 @@
 <!-- データベース接続 -->
 <?php
+// 関数
+// 1関数1機能←可読性・テストのしやすさ
+
+// 関数（データベース接続）
+// 引数：なし
+// 返り値：接続結果を返す
+function dbConnect(){
+// 接続に成功した場合
+try{
+  $dbh = new PDO($dsn,$user,$pass,[
+    // 例外を出力
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+  ]);
+// 接続に失敗した場合
+} catch(PDOException $e) {
+  echo '接続失敗'. $e->getMessage();
+  exit();
+};
+return $dbh;
+}
+
+
+// ①SQLの準備
+$sql = 'SELECT * FROM posts';
+// ②SQL実行
+$stmt = $dbh->query($sql);
+// ③SQLの結果を受け取る
+$result = $stmt->fetchall(pdo::FETCH_ASSOC);
+// var_dump($result);←取得データ確認コード
+
+// db接続終了
+$dbh = null;
+
+
+
 // db接続にDSN（data source name）ユーザー、パスワードを定義
 $dsn = 'mysql:host=localhost;dbname=recipes;charset=utf8';
 $user = 'recipes_user';
 $pass = 'Recipes1234';
 
-// 接続に成功した場合
-try{
-    $dbh = new PDO($dsn,$user,$pass,[
-      // 例外を出力
-      PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    ]);
-    // echo '接続成功';
-    // ①SQLの準備
-    $sql = 'SELECT * FROM posts';
-    // ②SQL実行
-    $stmt = $dbh->query($sql);
-    // ③SQLの結果を受け取る
-    $result = $stmt->fetchall(pdo::FETCH_ASSOC);
-    // var_dump($result);←取得データ確認コード
 
-    // db接続終了
-    $dbh = null;
-
-// 接続に失敗した場合
-} catch(PDOException $e) {
-    echo '接続失敗'. $e->getMessage();
-    exit();
-};
 
 ?>
 
