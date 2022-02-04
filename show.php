9c9add36d059a7d4e1765340d30f954f81f1ce8f
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-04 21:56:34 +0900

    Add: show-詳細画面雛形

---
+++ show.php
@@ -0,0 +1,21 @@
+<?php 
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>詳細画面</title>
+</head>
+<body>
+  <h2>詳細画面</h2>
+  <h3>タイトル：</h3>
+  <p>投稿日時：</p>
+  <p>カテゴリー：</p>
+  <hr>
+  <p></p>
+</body>
+</html>
