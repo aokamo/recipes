bf644e994844b798ef21cadc6407fd6759854797
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-05 21:09:23 +0900

    Fix: index-詳細リンク修正（ディレクトリ抜け）

--- index.php
+++ index.php
@@ -87,7 +87,7 @@ $postsData = getAllPosts();
             <td><?php echo $column['id']; ?></td>
             <td><?php echo $column['title']; ?></td>
             <td><?php echo $column['category_id']; ?></td>
-            <td><a href="/show.php?id=<?php echo $column['id']; ?>">詳細</a></td>
+            <td><a href="/recipes/show.php?id=<?php echo $column['id']; ?>">詳細</a></td>
           </tr>
           <?php endforeach; ?>
           <!-- endforeachにはセミコロン -->
