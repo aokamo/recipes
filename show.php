d8f2528f6fdfbf7940461dbbecadb725f8cc3c61
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-06 18:10:34 +0900

    Edit: show,dbc-データベース接続記述整理

--- show.php
+++ show.php
@@ -2,30 +2,7 @@
 
 require_once('dbc.php');
 
-  $id = $_GET['id'];
-  // 値の判定をして、空の値や不正な値への処理を追加
-  if(empty($id)){
-    exit('IDが不正です。');
-  }
-
-  $dbh = dbConnect();
-
-  // SQL準備
-  $stmt = $dbh->prepare('SELECT * FROM posts Where id = :id');
-    // id = （直書きしない）
-  $stmt->bindValue(':id', (int)$id, PDO::PARAM_INT);
-    // ↑↑プレースホルダで後から定義
-  // SQL実行
-  $stmt->execute();
-    // ↑↑prepare statementを実行するとき
-  // 結果を取得
-  $result = $stmt->fetch(PDO::FETCH_ASSOC);
-  // var_dump($result);
-
-  // 値がデータベースにない場合の処理
-  if(!$result){
-    exit('記事がありません。');
-  }
+$result = getPost($_get['id']);
 
 ?>
 
