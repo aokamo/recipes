6955b5601216375cbcd593351a556486b7cd0f6b
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-07 21:25:47 +0900

    Edit: form-フォーム画面完成

--- form.php
+++ form.php
@@ -4,9 +4,15 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>form</title>
 </head>
 <body>
-  
+  <h2>フォーム</h2>
+  <form action="index.php" method="POST"></form>
+    <p><label for="title-id">タイトル：</label></p>
+    <input type="text" name="title" id="title-id">
+    <p><label for="body-id">本文：</label></p>
+    <textarea name="body" id="body-id"></textarea>
+    <input type="submit" value="送信">
 </body>
 </html>
