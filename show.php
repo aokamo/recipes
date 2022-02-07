c17a24b10f3ed075e04a2e940822c2202eefadbf
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-07 22:03:22 +0900

    Edit: show-ヘッダー追加

--- show.php
+++ show.php
@@ -9,14 +9,19 @@ $result = getPost($_GET['id']);
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>詳細画面</title>
-</head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>詳細画面</title>
+    <!-- bootstrap -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+  </head>
 <body>
+  <header>
+    <?php include('navbar.php'); ?>
+  </header>
   <h2>詳細画面</h2>
-  <h3>タイトル：</h3>
+  <p>タイトル：</p>
   <p>投稿日時：</p>
   <p>カテゴリー：</p>
   <hr>
