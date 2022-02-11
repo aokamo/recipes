1dc49d2fa70b7ca778b748b64aef1b07dcc2495a
atsushi-okamoto <a.okamoto20@gmail.com>
2022-02-11 16:21:51 +0900

    Edit: post_create-バリデーション(null:false->title,body)(文字制限：title > 191)

--- post_create.php
+++ post_create.php
@@ -2,6 +2,16 @@
 
 $post = $_POST;
 
+if (empty($post['title'])){
+  exit('タイトルを入力してください');
+}
 
+if (mb_strlen($post['title']) > 191){
+  exit('191文字以下で入力してください');
+}
+
+if (empty($post['body'])){
+  exit('本文を入力してください');
+}
 
 ?>
